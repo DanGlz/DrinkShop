0e955c411db2e0c79cdca96f812d21704cb54d65
nitzan <nitzanmassad@gmail.com>
2017-06-27 10:40:37 +0300

    26.6.17

--- app/getAllProductsController.js
+++ app/getAllProductsController.js
@@ -5,7 +5,6 @@
 angular.module("myApp")
     .controller('getAllProductsController', ['getAllProductsService','$scope', function (getAllProductsService ,$scope) {
         let self = this;
-        console.log( "check");
         //self.Products = {DrinkName : nitz , DrinkName: robi}
 
         getAllProductsService.getAllProducts2().then(function (success) {
@@ -20,8 +19,10 @@ angular.module("myApp")
 
         self.propertyName = 'DrinkID';
         $scope.reverse = true;
+        $scope.src = "http://www.hinnawi.org.il/wp-content/uploads/%D7%91%D7%99%D7%A8%D7%94-%D7%92%D7%95%D7%9C%D7%93%D7%A1%D7%98%D7%90%D7%A8-%D7%9C%D7%90-%D7%9E%D7%A1%D7%95%D7%A0%D7%9F-330-%D7%91%D7%A7%D7%91%D7%95%D7%A7-280x280.png"
 
         $scope.sortBy = function(propertyName) {
+            console.log (propertyName)
             if (propertyName == self.propertyName ){
                 $scope.reverse =  !$scope.reverse
             }
